00b226346e8e2dac46b9109f04ff9c34412af09b
Michelle Cheng <cheng.michelley@gmail.com>
2026-04-28 21:38:57 -0400

    Setup

--- models/user.js
+++ models/user.js
@@ -9,10 +9,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    email: String,
-    phoneNumber: String,
-    address: String
-})
+});
 
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
-const testController = require('./controllers/test-jwt')
 const authController = require('./controllers/auth')
 const usersController = require('./controllers/users')
-const verifyJwt = require('./middleware/verify-jwt')
 
 require('./db/connection')
 
@@ -18,9 +16,7 @@ app.use(logger('dev'));
 
 // Routes
 
-app.use('/test-jwt', testController)
 app.use('/auth', authController)
-app.use(verifyJwt) //auth check
 app.use('/users', usersController)
 
 app.listen(3000, () => {
